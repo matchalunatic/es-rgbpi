2739c23b8117d3af3a637086e2a02c04716bb866
Matcha Luna <matchalunatic@gmail.com>
2019-08-18 22:36:46 +0200

    Added optional runcommand patch

--- utils/deploy-on-retropie.sh
+++ utils/deploy-on-retropie.sh
@@ -29,3 +29,20 @@ cd ..
 echo "Installing resolution management system in /opt/retropie/extras"
 mkdir /opt/retropie/extras/
 cp -nrv *.py rgbpi/ data/ /opt/retropie/extras
+fgrep runcommand_retroarch_append_config.sh /opt/retropie/supplementary/runcommand/runcommand.sh || for _ in 1; do
+    cat > /tmp/runcommand.patch <<'EOF'
+--- /tmp/runcommand.sh.a	2019-08-15 00:53:47.229302093 +0200
++++ /tmp/runcommand.sh.b	2019-08-15 00:53:51.949219789 +0200
+@@ -1057,6 +1057,7 @@
+     [[ -n "$GOVERNOR" ]] && set_governor "$GOVERNOR"
+ 
+     retroarch_append_config
++    user_script "runcommand_retroarch_append_config.sh"
+ 
+     # workaround for launching xserver on correct/user owned tty
+     # see https://github.com/RetroPie/RetroPie-Setup/issues/1805
+
+EOF
+    sudo patch -p0 /opt/retropie/supplementary/runcommand/runcommand.sh /tmp/runcommand.patch
+    rm /tmp/runcommand.patch
+done
